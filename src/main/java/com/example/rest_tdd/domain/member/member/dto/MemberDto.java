fbdc38d6ab6665c88e078831de9003b73f6bb8bd
linedj <eunjun998@gmail.com>
2025-02-04 15:24:12 +0900

    join r/g 4

--- src/main/java/com/example/rest_tdd/domain/member/member/dto/MemberDto.java
+++ src/main/java/com/example/rest_tdd/domain/member/member/dto/MemberDto.java
@@ -1,7 +1,6 @@
 package com.example.rest_tdd.domain.member.member.dto;
 
 import com.example.rest_tdd.domain.member.member.entity.Member;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -10,9 +9,7 @@ import java.time.LocalDateTime;
 public class MemberDto {
 
     private long id;
-    @JsonProperty("createdDatetime")
     private LocalDateTime createdDate;
-    @JsonProperty("modifiedDatetime")
     private LocalDateTime modifiedDate;
     private String nickname;
 

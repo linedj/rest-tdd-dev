0665f3ee50839235b7ffd1a0a2c241876735dfe0
linedj <eunjun998@gmail.com>
2025-02-05 12:40:08 +0900

    post write -r

--- src/main/java/com/example/rest_tdd/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.rest_tdd.domain.post.post.repository;
 import com.example.rest_tdd.domain.post.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findTopByOrderByIdDesc();
 }

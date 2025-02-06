100e1407ea94be4eba6e3fc69c9957f63bcc3eb0
linedj <eunjun998@gmail.com>
2025-02-06 12:03:29 +0900

    sample data fix

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
@@ -2,7 +2,7 @@ package com.example.rest_tdd.domain.post.post.controller;
 
 import com.example.rest_tdd.domain.member.member.entity.Member;
 import com.example.rest_tdd.domain.post.post.dto.PageDto;
-import com.example.rest_tdd.domain.post.post.dto.PostDto;
+import com.example.rest_tdd.domain.post.post.dto.PostWithContentDto;
 import com.example.rest_tdd.domain.post.post.entity.Post;
 import com.example.rest_tdd.domain.post.post.service.PostService;
 import com.example.rest_tdd.global.Rq;
@@ -38,7 +38,7 @@ public class ApiV1PostController {
     }
 
     @GetMapping("{id}")
-    public RsData<PostDto> getItem(@PathVariable long id) {
+    public RsData<PostWithContentDto> getItem(@PathVariable long id) {
 
         Post post = postService.getItem(id).orElseThrow(
                 () -> new ServiceException("404-1", "존재하지 않는 글입니다.")
@@ -52,7 +52,7 @@ public class ApiV1PostController {
         return new RsData<>(
                 "200-1",
                 "%d번 글을 조회하였습니다.".formatted(id),
-                new PostDto(post)
+                new PostWithContentDto(post)
         );
     }
 
@@ -63,7 +63,7 @@ public class ApiV1PostController {
     }
 
     @PostMapping
-    public RsData<PostDto> write(@RequestBody @Valid WriteReqBody reqBody) {
+    public RsData<PostWithContentDto> write(@RequestBody @Valid WriteReqBody reqBody) {
 
         Member actor = rq.getAuthenticatedActor();
         Post post = postService.write(actor, reqBody.title(), reqBody.content(), reqBody.published(), reqBody.listed());
@@ -71,7 +71,7 @@ public class ApiV1PostController {
         return new RsData<>(
                 "201-1",
                 "%d번 글 작성이 완료되었습니다.".formatted(post.getId()),
-                new PostDto(post)
+                new PostWithContentDto(post)
         );
     }
 
@@ -79,7 +79,7 @@ public class ApiV1PostController {
     }
 
     @PutMapping("{id}")
-    public RsData<PostDto> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody reqBody) {
+    public RsData<PostWithContentDto> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody reqBody) {
 
         Member actor = rq.getAuthenticatedActor();
 
@@ -93,7 +93,7 @@ public class ApiV1PostController {
         return new RsData<>(
                 "200-1",
                 "%d번 글 수정이 완료되었습니다.".formatted(id),
-                new PostDto(post)
+                new PostWithContentDto(post)
         );
     }
 
